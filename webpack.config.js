f5e2ff7804a7dc93f7de14dbc071cebe4caa9f13
akm <akm2000@gmail.com>
2017-12-13 19:12:15 +0900

    :up: Add config to load css

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,14 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
     ]
   },
   devServer: {
