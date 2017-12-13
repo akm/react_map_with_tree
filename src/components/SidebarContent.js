d94121cc1ee82a6be89b0261c60062bff5bf4b01
akm <akm2000@gmail.com>
2017-12-13 19:34:09 +0900

    :+1: Add CheckboxTree to SidebarContent
    See https://github.com/jakezatecky/react-checkbox-tree

--- src/components/SidebarContent.js
+++ src/components/SidebarContent.js
@@ -2,21 +2,13 @@ import React from 'react';
 import MaterialTitlePanel from './MaterialTitlePanel';
 import PropTypes from 'prop-types';
 
+import 'react-checkbox-tree/lib/react-checkbox-tree.css';
+import CheckboxTree from 'react-checkbox-tree';
+
 const styles = {
   sidebar: {
     width: 256,
     height: '100%',
-  },
-  sidebarLink: {
-    display: 'block',
-    padding: '16px 0px',
-    color: '#757575',
-    textDecoration: 'none',
-  },
-  divider: {
-    margin: '8px 0',
-    height: 1,
-    backgroundColor: '#757575',
   },
   content: {
     padding: '16px',
@@ -25,16 +17,44 @@ const styles = {
   },
 };
 
-const SidebarContent = (props) => {
-  return (
-    <MaterialTitlePanel title="Menu" style={styles.sidebar}>
-      <div style={styles.content}>
-        <a href="index.html" style={styles.sidebarLink}>Home</a>
-        <div style={styles.divider} />
-      </div>
-    </MaterialTitlePanel>
-  );
-};
+const nodes = [{
+    value: 'mars',
+    label: 'Mars',
+    children: [
+        { value: 'phobos', label: 'Phobos' },
+        { value: 'deimos', label: 'Deimos' },
+    ],
+}];
+
+const state = {
+  checked: [],
+  expanded: [],
+}
+
+class SidebarContent extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      checked: [],
+      expanded: [],
+    };
+  }
+
+  render() {
+    return (
+      <MaterialTitlePanel title="Menu" style={styles.sidebar}>
+        <CheckboxTree
+          nodes={nodes}
+          checked={this.state.checked}
+          expanded={this.state.expanded}
+          onCheck={checked => this.setState({ checked })}
+          onExpand={expanded => this.setState({ expanded })}
+        />
+      </MaterialTitlePanel>
+    )
+  }
+}
 
 SidebarContent.propTypes = {
   style: PropTypes.object,
